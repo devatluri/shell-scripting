82440b1e405c54f92abe5e070553d8b0a6ef6912
Devendar Attluri <devatluri@gmail.com>
2023-08-21 22:17:31 +0530

    addnl changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,12 @@
 
 echo "Please enter your username"
 
-read USERNAME #This is the user input at the terminal
+read -s USERNAME #This is the user input at the terminal
 
 echo "Username entered is : $USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD
+
+echo "Password entered is $PASSWORD"
