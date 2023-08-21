8cbe72dd3152094d4948ec05b0cc50f91b2dcd1e
Devendar Attluri <devatluri@gmail.com>
2023-08-21 21:58:28 +0530

    addnl changes

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#this is test script
+echo "Hello world"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
