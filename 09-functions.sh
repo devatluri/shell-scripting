b403ec60eef94deb85e504c5facf3b78d5f9d9f8
Devendar Attluri <devatluri@gmail.com>
2023-09-16 08:19:59 +0530

    Update 09-functions.sh

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,6 +4,12 @@
 
 USERID=$(id -u)
 
+if [ $USERID -ne 0 ]
+then    
+    echo "ERROR: Please run with root access"
+    exit 1
+fi
+
 #This function should validate previous command and inform user SUCCESS or FAILURE
 VALIDATE(){
     #$1 --> it will receive arugument1, $2 is argument 2
