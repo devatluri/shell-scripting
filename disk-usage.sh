eb1019c5c7bbe75cdfc033d3a318a9ac83a7fcc5
Devendar Attluri <devatluri@gmail.com>
2023-09-16 19:47:22 +0530

    Update disk-usage.sh

--- disk-usage.sh
+++ disk-usage.sh
@@ -20,7 +20,6 @@ message=""
 while IFS= read line
 do
     usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-    echo "$usage \n"
     partition=$(echo $line | awk '{print $1}')
     if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
