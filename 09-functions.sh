6b23c6f16b1b6d3795828e6a9c9a961774ed563c
Devendar Attluri <devatluri@gmail.com>
2023-09-16 09:14:59 +0530

    Update 09-functions.sh

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-DATE=$(date +%F:%H:%M:%S)
-LOGFILE=/tmp/
+SCRIPT_NAME=$0
+DATE=$(date +%F)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 #TO intall MySQL
 
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then    
-    echo "ERROR: Please run with root access"
-    exit 1
-fi
-
-#This function should validate previous command and inform user SUCCESS or FAILURE
 VALIDATE(){
     #$1 --> it will receive arugument1, $2 is argument 2
     if [ $? ne 0 ]
@@ -25,8 +16,19 @@ VALIDATE(){
     fi
 }
 
-yum install mysql -y
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then    
+    echo "ERROR: Please run with root access"
+    exit 1
+fi
+
+#This function should validate previous command and inform user SUCCESS or FAILURE
+
+
+yum install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing POSTFIX"
