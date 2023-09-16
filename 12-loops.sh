1bd772ff3df2a189e2a88f7f65c7f305022b71e0
Devendar Attluri <devatluri@gmail.com>
2023-09-16 15:15:40 +0530

    Update 12-loops.sh

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+R="\e[31m]"
+G="\e[0m]"
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+
+#all arguments are in $@
 for i in $@
 do
     yum install $i -y
