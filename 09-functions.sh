76d87921793b70fe5ae7fd349ff00a6d093922d4
Devendar Attluri <devatluri@gmail.com>
2023-09-16 08:18:33 +0530

    Create 09-functions.sh

---
+++ 09-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#TO intall MySQL
+
+USERID=$(id -u)
+
+#This function should validate previous command and inform user SUCCESS or FAILURE
+VALIDATE(){
+    #$1 --> it will receive arugument1, $2 is argument 2
+    if [ $? ne 0 ]
+    then
+        echo "Installation .... FAILURE"
+        exit 1
+    else
+        echo "Installation .... SUCCESS"
+fi
+}
+
+yum install mysql -y
+VALIDATE $?
+
+yum install postfix -y
+VALIDATE $?
