a1e126c6dfaeeb2b64756a4dac287f6db49e1ee7
Devendar Attluri <devatluri@gmail.com>
2023-09-16 19:32:25 +0530

    Update disk-usage.sh

--- disk-usage.sh
+++ disk-usage.sh
@@ -10,10 +10,11 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem' )
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs' )
 DISK_USAGE_THRESHOLD=1
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6}')
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1})
 done <<< $DISK_USAGE
